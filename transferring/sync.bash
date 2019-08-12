c50aa4929fb434d76fad7397736cec23ffd95ce7
Ben Rowan <me@ben-rowan.com>
2019-08-12 12:13:58 +0100

    Add fix for rsync mtime comparison not working correctly when syncing files

--- transferring/sync.bash
+++ transferring/sync.bash
@@ -17,4 +17,7 @@ readonly to="${2}";
 # The speed has been limited because otherwise the device seems to get 'overloaded' and
 # stops working.
 
-rsync -rP --delete --bwlimit=1.5m "${from}" "/media/${USER}/WALKMAN/${to}";
+# --times has been set to ensure that mtimes are maintained when copying files. --modify-window=1 has been set
+# to account for the FAT32 filesystem not maintaining mtimes with the same accuracy as ext4
+
+rsync -rP --modify-window=1 --times --delete --bwlimit=1.5m "${from}" "/media/${USER}/WALKMAN/${to}";

5f4fb58791cae6a8c0f93f6dd2cf0011b6f67c30
Ben Rowan <me@ben-rowan.com>
2019-08-13 09:53:43 +0100

    Add exclude for JPG files and prune empty directories

--- transferring/sync.bash
+++ transferring/sync.bash
@@ -12,12 +12,16 @@ then
 fi
 
 readonly from="${1}";
-readonly to="${2}";
+readonly to="/media/${USER}/WALKMAN/${2}";
+
 
 # The speed has been limited because otherwise the device seems to get 'overloaded' and
 # stops working.
 
 # --times has been set to ensure that mtimes are maintained when copying files. --modify-window=1 has been set
-# to account for the FAT32 filesystem not maintaining mtimes with the same accuracy as ext4
+# to account for the FAT32 filesystem not maintaining mtimes with the same accuracy as ext4.
 
-rsync -rP --modify-window=1 --times --delete --bwlimit=1.5m "${from}" "/media/${USER}/WALKMAN/${to}";
+# Add exclude for *.jpg so we're not syncing image files which the MP3 player can't display. This means we can
+# then also --prune-empty-dirs and remove all empty directories from the player so you don't waste time looking in them.
+
+rsync -rP --prune-empty-dirs --modify-window=1 --times --delete --bwlimit=1.5m --exclude '*.jpg' "${from}" "${to}";
